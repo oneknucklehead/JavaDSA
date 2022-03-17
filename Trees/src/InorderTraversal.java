b826ef52385d1b5c2363be852f8f53c4c0effcc4
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-03-17 15:14:48 +0530

    morris traversal inorder added

--- Trees/src/InorderTraversal.java
+++ Trees/src/InorderTraversal.java
@@ -32,6 +32,31 @@ class Solution {
         return ans;
     }
 }
+    // Morris traversal Best solution TC: O(N) SC: O(1)
+//    ArrayList<Integer> list = new ArrayList<>();
+//    TreeNode curr = root;
+//        while(curr!=null){
+//                if(curr.left==null){
+//                list.add(curr.val);
+//                curr=curr.right;
+//                }
+//                else{
+//                TreeNode pre = curr.left;
+//                while((pre.right!=null)&&(pre.right!=curr)){
+//                pre=pre.right;
+//                }
+//                if(pre.right==null){
+//                pre.right=curr;
+//                curr=curr.left;
+//                }
+//                else{
+//                pre.right=null;
+//                list.add(curr.val);
+//                curr=curr.right;
+//                }
+//                }
+//                }
+//                return list;
 
 //recursive solution
 // ArrayList<Integer> list = new ArrayList<Integer>();
